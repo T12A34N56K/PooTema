2ada4b35f7601c0cefdfd5787ce02725bc9a7f8b
T12A34N56K <andrei.barbu2005@gmail.com>
2025-05-02 14:58:14 +0300

    O clasa derivata din bot pentru un lunetist.

---
+++ glbRiflemen.h
@@ -0,0 +1,24 @@
+#ifndef glbRiflemen
+#define glbRiflemen
+#include "glbBot.h"
+#include "glbData.h"
+
+class Riflemen: public Bot{
+public:
+    Riflemen(int x, int y):  Bot(300, 300, 2, 5, 90, 30, 10, 30, 0, x, y, 1){}
+    void mns(int xn ,int yn, std::vector<std::vector<int*>>&pos, Bot* tinta){
+        Bot::move(xn, yn, pos);
+        Bot::shoot(tinta);
+    }
+    ~Riflemen(){
+        std::vector<std::vector<int*>> &pos = *Data::getData().Pos();
+        pos[id][0] = new int(101);
+        pos[id][1] = new int(101);
+        if(id<8)
+            Data::getData().Team1()->erase(find(Data::getData().Team1()->begin(), Data::getData().Team1()->end(), this));
+        else
+            Data::getData().Team2()->erase(find(Data::getData().Team2()->begin(), Data::getData().Team2()->end(), this));
+    }
+};
+
+#endif
